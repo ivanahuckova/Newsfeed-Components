4bb898b2c4b18ff18d7417d3294cbbcde5f7c606
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-30 15:22:33 +0100

    feat: Pimp up the animation, add yoyo effect

--- Animation/Animation.js
+++ Animation/Animation.js
@@ -1,5 +1,5 @@
 let img = document.querySelector("img#animationImg");
 
 document.querySelector("img.menu-button").addEventListener("click", function() {
-	TweenMax.to(img, 8, { top: -500 });
+	TweenMax.to(img, 5, { top: -200, repeat: 1, ease: Power2.easeInOut, yoyo: true });
 });
