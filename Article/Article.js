e687fd392ef1d3b47693f77550a4a2d68aea2765
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-30 14:04:10 +0100

    feat: Add automatic closing article functionality when opened new article

--- Article/Article.js
+++ Article/Article.js
@@ -14,12 +14,10 @@ class Article {
 
 	expandArticle() {
 		// Using our reference to the domElement, toggle a class to expand or hide the article.
-		this.domElement.classList.toggle("article-open");
-		if (this.domElement.classList.contains("article-open")) {
-			this.expandButton.textContent = "shrink";
-		} else {
-			this.expandButton.textContent = "expand";
+		for (let i = 0; i < articles.length; i++) {
+			articles[i].classList.remove("article-open");
 		}
+		this.domElement.classList.toggle("article-open");
 	}
 }
 //<====>
